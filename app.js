c00338b8f2703a9913305eee8ccbf13c44e30fdb
Sam <milk3531@hotmail.com>
2021-12-31 15:52:47 -0800

    finished copying project. Long way to go!

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ import {
     Hill
 } from './hill.js';
 
+import { 
+    SheepController }
+ from './sheep-controller.js';
+
+ import {
+     Sun
+ } from './Sun.js';
+
+
+
 class App{
     constructor() {
         this.canvas = document.createElement('canvas');
@@ -9,8 +19,15 @@ class App{
         document.body.appendChild(this.canvas);
 
         this.hills = [
+            new Hill('#fd6bea',0.2,12),
+            new Hill('#ff59c2',0.5,8),
             new Hill('#ff4674', 1.4,6)
+            
         ];
+
+        this.SheepController = new SheepController();
+
+        this.sun = new Sun();
         
         window.addEventListener('resize', this.resize.bind(this),false);
         //Resize?
@@ -27,21 +44,29 @@ class App{
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2,2);
 
+        this.sun.resize(this.stageWidth, this.stageHeight);
+
         for (let i = 0; i < this.hills.length; i++) {
             this.hills[i].resize(this.stageWidth, this.stageHeight);
         }
+
+        this.SheepController.resize(this.stageWidth, this.stageHeight);
     }
 
     animate(t) {
         requestAnimationFrame(this.animate.bind(this));
 
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-        // Erases current canvas
+        
+        this.sun.draw(this.ctx,t);
+
 
         let dots;
         for (let i = 0; i < this.hills.length; i++){
             dots = this.hills[i].draw(this.ctx);
         }
+
+        this.SheepController.draw(this.ctx,t,dots);
     }
 }
 window.onload = () => {
